94b2a6d0396357636ffe158d9e91dc596175e3b4
Rodrigo Pinesso <142932782+rodrigopinesso@users.noreply.github.com>
2025-04-15 20:42:24 -0300

    Delete src directory

--- src/main/java/com/example/pessoa_trabalho/repository/TrabalhoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.pessoa_trabalho.repository;
-
-import com.example.pessoa_trabalho.model.Trabalho;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrabalhoRepository extends JpaRepository<Trabalho, Long> {}
